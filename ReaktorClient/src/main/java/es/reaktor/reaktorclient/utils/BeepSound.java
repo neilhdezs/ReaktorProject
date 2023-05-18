59a7b1785eb9c98ce7f3a88c0d337b84aaf25f56
aaljcas <aaljcas1808@g.educaand.es>
2023-05-18 20:44:45 +0200

    add the Beep proyect and execute the comand to hear the "Beep"

---
+++ ReaktorClient/src/main/java/es/reaktor/reaktorclient/utils/BeepSound.java
@@ -0,0 +1,38 @@
+package es.reaktor.reaktorclient.utils;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import es.reaktor.reaktorclient.utils.exceptions.ReaktorClientException;
+
+import java.io.File;
+
+public class BeepSound extends Thread
+{
+
+    /**
+     * - Logger -
+     * This logger is used to log the information of the application
+     */
+    private static final Logger LOGGER = LogManager.getLogger();
+
+    private final CommandExecutor commandExecutor;
+
+    public BeepSound(CommandExecutor commandExecutor)
+    {
+        this.commandExecutor = commandExecutor;
+    }
+
+    @Override
+    public void run()
+    {
+        try
+        {
+            this.commandExecutor.executeCommand(getClass().getResource(".\\Debug\\net6.0\\BeepReaktor.exe").getPath());
+        }
+        catch (ReaktorClientException reaktorClientException)
+        {
+            LOGGER.warn("Error this comand doesn`t work", reaktorClientException);
+        }
+    }
+}
